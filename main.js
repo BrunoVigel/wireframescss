aa94b6610bf44570514ba694e6a1743908da842f
BrunoVigel <brunorosav@gmail.com>
2021-08-02 15:50:08 -0300

    continuacao desktop

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 var swiper = new Swiper('.mySwiper', {
+  autoplay: {
+    delay: 2000
+  },
   pagination: {
     el: '.swiper-pagination'
-  }
+  },
+  autoHeight: true
+})
+
+const btn = document.querySelector('.scroll-down')
+const section = document.querySelector('#products')
+btn.addEventListener('click', function () {
+  window.scrollTo({
+    top: section.offsetTop,
+    behavior: 'smooth'
+  })
 })
