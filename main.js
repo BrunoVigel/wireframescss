1726c2fecb653fff3e96616aaf91a106d73313aa
BrunoVigel <brunorosav@gmail.com>
2021-08-09 17:35:35 -0300

    header

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ var swiper = new Swiper('.mySwiper', {
   autoHeight: true
 })
 
+var swiper2 = new Swiper('.product-slider', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+})
+
 const btn = document.querySelector('.scroll-down')
 const section = document.querySelector('#products')
 btn.addEventListener('click', function () {
@@ -16,3 +23,28 @@ btn.addEventListener('click', function () {
     behavior: 'smooth'
   })
 })
+
+// Abre e fecha o menu quando clicar no icone: hamburguer e x
+const nav = document.querySelector('nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+window.onscroll = function () {
+  myFunction()
+}
+
+let header = document.querySelector('header')
+let sticky = header.offsetTop
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add('sticky')
+  } else {
+    header.classList.remove('sticky')
+  }
+}
